537a1490306f4906827497adb54922420a515116
Lucas Schweickert <cyberfly@gmx.net>
2026-05-12 20:45:25 +0200

    extremely simple file server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Cyberfly100/bootdev_httpserver
+
+go 1.26.2
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	log.Fatal(server.ListenAndServe())
+}
