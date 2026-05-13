985fa6354c9986e807ffe7d9f3d5f62349475683
Lucas Schweickert <cyberfly@gmx.net>
2026-05-13 11:39:39 +0200

    added readiness endpoint

--- main.go
+++ main.go
@@ -11,6 +11,11 @@ func main() {
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	log.Fatal(server.ListenAndServe())
 }
