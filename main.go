abde25013e09c0d3997008ebaa2c16cb53f4eece
Lucas Schweickert <cyberfly@gmx.net>
2026-05-19 13:28:35 +0200

    added db

--- main.go
+++ main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/Cyberfly100/bootdev_httpserver/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Could not connect to database: %v", err)
+	}
+	defer dbConn.Close()
+
 	const port = "8080"
 
-	apiCfg := &apiConfig{}
+	apiCfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
+		db:             database.New(dbConn),
+	}
 	mux := http.NewServeMux()
 
 	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
